0c19210d8838d11382f597d6ad140a2a818cede4
Eduardo Dias Souza Freire <eduardodiassouzafreire@gmail.com>
2024-10-24 17:07:10 -0300

    Adição dos Controladores

--- obj/Debug/net8.0/Projeto_Casa_Criancas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto_Casa_Criancas.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Casa_Criancas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9e5eaaaca685dcf9281df4815d7a888e1a6614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Casa_Criancas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Casa_Criancas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
